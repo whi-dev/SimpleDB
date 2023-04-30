23dd775de3dfb88b8baac87a743d32a43fdb79e8
whi <whidev1938@gmail.com>
2023-04-30 10:57:09 +0900

    ファイルマネジャー実装
    char.3の練習問題を統計情報以外は実装した
    FileManager.appendでsize()だったのをnumBlockに変更した。多分大丈夫。

--- app/src/main/java/SimpleDB/file_manager/FileManager.java
+++ app/src/main/java/SimpleDB/file_manager/FileManager.java
@@ -1,6 +1,7 @@
 package SimpleDB.file_manager;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,8 +27,68 @@ public class FileManager {
       }
     }
   }
-  public synchronized void read(BlockID id, Page p) {
-    
+  public synchronized void read(BlockID blk, Page p) {
+    try{
+      RandomAccessFile f = getFile(blk.fileName());
+      f.seek(blk.number() * blockSize);
+      f.getChannel().read(p.contents());
+    }catch(IOException e) {
+      throw new RuntimeException("cannot read block " + blk);
+    }
   }
-
+  public synchronized void write(BlockID blk, Page p)
+  {
+    try{
+      RandomAccessFile f = getFile(blk.fileName());
+      f.getChannel().read(p.contents());
+      f.seek(blk.number() * blockSize);
+      f.getChannel().write(p.contents());
+    }catch(IOException e) {
+      throw new RuntimeException("cannot write block " + blk);
+    }
+  }
+  public synchronized BlockID append(String filename) {
+    int newBlkIdx = numBlock(filename);
+    BlockID blk = new BlockID(filename, newBlkIdx);
+    byte[] b = new byte[blockSize];
+    try {
+      RandomAccessFile f = getFile(blk.fileName());
+      f.seek(blk.number() * blockSize);
+      f.write(b);
+    }catch(IOException e) {
+      throw new RuntimeException("cannot append block " + blk);
+    }
+    return blk;
+  }
+  public int length(String filename) {
+    try {
+      RandomAccessFile f = getFile(filename);
+      return (int)(f.length());
+    }catch(IOException e) {
+      throw new RuntimeException("cannot access " + filename);
+    }
+  }
+  public boolean isNew() {
+    return isNew;
+  }
+  public int blockSize() {
+    return blockSize;    
+  }
+  public int numBlock(String filename) {
+    try {
+      RandomAccessFile f = getFile(filename);
+      return (int)f.length() / blockSize;
+    }catch(IOException e) {
+      throw new RuntimeException("cannot access " + filename);
+    }  
+  }
+  private RandomAccessFile getFile(String filename) throws IOException{
+    RandomAccessFile f = openFiles.get(filename);
+    if(f == null) {
+      File dbTable = new File(dbDirectory, filename);
+      f = new RandomAccessFile(dbTable, "rws");
+      openFiles.put(filename, f);
+    }
+    return f;
+  }
 }
--- app/src/main/java/SimpleDB/file_manager/Page.java
+++ app/src/main/java/SimpleDB/file_manager/Page.java
@@ -3,13 +3,16 @@ package SimpleDB.file_manager;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class Page {
   private ByteBuffer bb;
+  private int blockSize;
   public static final Charset CHARSET = StandardCharsets.US_ASCII;
 
   public Page(int blocksize) {
     bb = ByteBuffer.allocateDirect(blocksize);
+    this.blockSize = blocksize;
   }
   public Page(byte[] b) {
     bb = ByteBuffer.wrap(b);
@@ -20,6 +23,27 @@ public class Page {
   public void setInt(int offset, int n) {
     bb.putInt(offset, n);
   }
+  public short getShort(int offset) {
+    return bb.getShort(offset);
+  }
+  public void setShort(int offset, short n) {
+    bb.putShort(offset, n);
+  }
+  public boolean getBoolean(int offset) {
+    char b = bb.getChar(offset);
+    return b == '\0';
+  }
+  public void setBoolean(int offset, boolean b) {
+    char c = b ? 1 : '\0';
+    bb.putChar(offset, c);
+  }
+  public Date getDate(int offset) {
+    long epoch = bb.getLong(offset);
+    return new Date(epoch);
+  }
+  public void setDate(int offset, Date d) {
+    bb.putLong(offset, d.getTime());  
+  }
   public byte[] getBytes(int offset) {
     bb.position(offset);
     int length = bb.getInt();
@@ -27,19 +51,33 @@ public class Page {
     bb.get(b);
     return b;
   }
-  public void setBytes(int offset, byte[] b) {
+  public void setBytes(int offset, byte[] b) throws RuntimeException{
+    if(offset + b.length > blockSize) {
+      throw new RuntimeException("overflow");
+    }
     bb.position(offset);
     bb.putInt(b.length);
     bb.put(b);
   }
   public String getString(int offset) {
-    byte[] b = getBytes(offset);
-    return new String(b, CHARSET);
+    byte[] str = new byte[blockSize];
+    int idx = 0;
+    char c;
+    while((c = bb.getChar(offset++)) != '\0') {
+      str[idx++] = (byte)c;
+    }
+    return new String(str, 0, idx, CHARSET);
   }
-  public void setString(int offset, String s) {
+  public void setString(int offset, String s) throws RuntimeException{
     byte[] b = s.getBytes(CHARSET);
-    setBytes(offset, b);
+    if(offset + b.length > blockSize) {
+      throw new RuntimeException("overflow");
+    }
+    bb.position(offset);
+    bb.put(b);
+    bb.putChar(offset + b.length, '\0');
   }
+
   public static int maxLength(int strlen) {
     float bytesPerChar = CHARSET.newEncoder().maxBytesPerChar();
     // length + btyes of String
