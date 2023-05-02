246bfd7b652364470b25e3ebc350dd58101c081e
whi <watanabehitoshi@watanabehitoshinoMacBook-Air.local>
2023-05-02 18:40:09 +0900

    BufferManager実装
    バッファ戦略はナイーブアルゴリズムなので、これからLRUに修正する
    flushAllのトランザクション番号うんぬんがあまりわかっていないのでもっと読み込む

--- app/src/main/java/SimpleDB/buffer/BufferManager.java
+++ app/src/main/java/SimpleDB/buffer/BufferManager.java
@@ -1,7 +1,8 @@
 package SimpleDB.buffer;
 
 import SimpleDB.log.LogManager;
-
+import SimpleDB.exception.BufferAbortException;
+import SimpleDB.file.BlockID;
 import SimpleDB.file.FileManager;
 
 public class BufferManager {
@@ -25,6 +26,64 @@ public class BufferManager {
         buff.flush();
       }
     }
+  }
+  public synchronized void unpin(Buffer buf) {
+    buf.unpin();
+    if(!buf.isPinned()) {
+      numAvailable++;
+      notifyAll();
+    }
+  }
+  public synchronized Buffer pin(BlockID blk) throws InterruptedException{
+    try {
+      long timestamp = System.currentTimeMillis();
+      Buffer buf = tryToPin(blk);
+      if(buf == null && waitTooLong(timestamp)) {
+        wait(MXA_TIME);
+        tryToPin(blk);
+      }
+      if(buf == null) {
+        throw new BufferAbortException(); 
+      }
+      return buf;
+    } catch(InterruptedException e) {
+      throw new BufferAbortException(); 
+    }
+  }
+  private boolean waitTooLong(long timestamp) {
+    return System.currentTimeMillis() - timestamp > MXA_TIME;
+  }
+  private Buffer tryToPin(BlockID blk) {
+    Buffer buf = findExistingBuffer(blk);
+    if(buf == null) {
+      buf = chooseUnpinnedBuffer(blk);
+      if(buf == null) {
+        return null;
+      }
+      buf.assignToBlock(blk);
+    }
+    if(!buf.isPinned()) {
+      numAvailable--;
+    }
+    buf.pin();
+    return buf;
+  }
+  private Buffer findExistingBuffer(BlockID blk) {
+    for(var buf : bufferpool) {
+      BlockID b = buf.block();
+      if(b != null && b.equals(blk)) {
+        return buf;
+      }
+    }
+    return null;
+  }
+  private Buffer chooseUnpinnedBuffer(BlockID blk) {
+    for(var buf : bufferpool) {
+      if(!buf.isPinned()) {
+        return buf;
+      }
+    }
+    return null;
   }
 
 }
---
+++ app/src/main/java/SimpleDB/exception/BufferAbortException.java
@@ -0,0 +1,5 @@
+package SimpleDB.exception;
+
+public class BufferAbortException extends InterruptedException{
+    
+}
